a935e68253e9dede84731fb44f0890a419ccf655
Paul Campbell <pacampbell91@gmail.com>
2020-08-20 15:25:47 +0200

    add pkgdown site and more helper functions

--- R/epicurve.R
+++ R/epicurve.R
@@ -14,12 +14,15 @@
 #'  default "non_missing" will take the sum of all non-missing values in the column.
 #' @param prop_line_colour colour of the proportion line. defaults to "red"
 #' @param prop_line_size width of the proportion line. defaults to 0.8
-#' @param floor_date_week should `date_col` dates be floored to the Monday of the ISO week they fall in? defaults to FALSE
+#' @param floor_date_week should `date_col` dates be floored to the Monday of the ISO week they fall in? defaults to TRUE
+#' @param label_weeks label primary date axis with week numbers? defaults to TRUE
+#' @param week_start day of week defined as the start of the week. defaults to "Monday" (ISO week standard)
 #' @param date_breaks date break intervals passed to [`ggplot2::scale_x_date`]. defaults to "2 weeks"
 #' @param date_labels [`base::strptime`] date label code passed to [`ggplot2::scale_x_date`]. defaults to "\\%V" (ISO Week)
 #' @param sec_date_axis plot a secondary date axis using default calculated ggplot2 date breaks and labels? defaults to FALSE
 #' @param facet_nrow nrow argument passed to [`ggplot2::facet_wrap`]
 #' @param facet_ncol ncol argument passed to [`ggplot2::facet_wrap`]
+#' @param facet_labs facet labeller argument passed to [`ggplot2::facet_wrap`]. Defaults to [`label_wrap_gen(width = 25)`]
 #' @param facet_lab_pos facet label position argument passed to strip.position in [`ggplot2::facet_wrap`]. defaults to "top". Options are `c("top", "bottom", "left", "right")`
 #' @param group_na_colour colour for missing values in `group_col`. defaults to "grey"
 #' @param title optional title for the plot
@@ -61,14 +64,18 @@ plot_epicurve <- function(df,
                           prop_col = NULL,
                           prop_numer = NULL,
                           prop_denom = "non_missing",
-                          prop_line_colour = "red",
+                          prop_line_colour = "black",
                           prop_line_size = 0.8,
                           floor_date_week = FALSE,
+                          label_weeks = FALSE,
+                          week_start = "Monday",
                           date_breaks = "2 weeks",
                           date_labels = "%V",
                           sec_date_axis = FALSE,
                           facet_nrow = NULL,
                           facet_ncol = NULL,
+                          facet_scales = "fixed",
+                          facet_labs = label_wrap_gen(width = 25),
                           facet_lab_pos = "top",
                           group_na_colour = "grey",
                           title = waiver(),
@@ -82,12 +89,22 @@ plot_epicurve <- function(df,
   g_vars_2 <- dplyr::enquos(facet_col, date_col)
 
   if(floor_date_week) {
-    df <- df %>% dplyr::mutate({{date_col}} := set_epi_week({{date_col}}))
+    week_start <- match.arg(week_start, c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"), several.ok = FALSE)
+    week_start <- switch (week_start,
+                          Monday = 1,
+                          Tuesday = 2,
+                          Wednesday = 3,
+                          Thursday = 4,
+                          Friday = 5,
+                          Saturday = 6,
+                          Sunday = 7)
+    df <- df %>% dplyr::mutate({{date_col}} := floor_week({{date_col}}, week_start = week_start))
+    #df <- df %>% dplyr::mutate({{date_col}} := aweek::date2week({{date_col}}, week_start = week_start))
   }
 
   df_epicurve <- df %>%
     dplyr::count(!!!g_vars_1) #%>%
-    #tidyr::drop_na()
+  #tidyr::drop_na()
 
   if (!missing(prop_col)) {
     if (is.null(prop_numer) | is.null(prop_denom))
@@ -98,7 +115,7 @@ plot_epicurve <- function(df,
       dplyr::group_by(!!!g_vars_2) %>%
       dplyr::summarise(
         n = dplyr::n(),
-        denom = ifelse(prop_denom == "non_missing", n, sum({{prop_col}} %in% prop_denom)),
+        denom = ifelse(prop_denom == "non_missing", sum(!is.na({{prop_col}})), sum({{prop_col}} %in% prop_denom)),
         num = sum({{prop_col}} %in% prop_numer, na.rm = TRUE),
         prop = num / denom,
         .groups = "drop"
@@ -125,35 +142,62 @@ plot_epicurve <- function(df,
     p <- p + geom_col(aes(fill = {{group_col}}), colour = "white", size = 0.2)
   }
 
-  if(sec_date_axis) {
-    p <- p + scale_x_date(date_breaks = date_breaks, date_labels = date_labels, sec.axis = ggplot2::sec_axis(trans = ~ .), expand = expansion(mult = c(0.01, 0.01)))
+  if(label_weeks) {
+    date_vec <- df %>% tidyr::drop_na({{date_col}}) %>% dplyr::arrange({{date_col}}) %>% dplyr::pull({{date_col}})
+    x_breaks <- seq.Date(date_vec[1], date_vec[length(date_vec)], by = date_breaks)
+    x_labs <- aweek::date2week(x_breaks, week_start = week_start, numeric = TRUE)
   } else {
-    p <- p + scale_x_date(date_breaks = date_breaks, date_labels = date_labels, expand = expansion(mult = c(0.01, 0.01)))
+    x_breaks <- waiver()
+    x_labs <- waiver()
   }
+
+  if(sec_date_axis) {
+    p <- p + scale_x_date(breaks = x_breaks, labels = x_labs, sec.axis = ggplot2::sec_axis(trans = ~ .), expand = expansion(mult = c(0.01, 0.01)))
+  } else {
+    p <- p + scale_x_date(breaks = x_breaks, labels = x_labs, expand = expansion(mult = c(0.01, 0.01)))
+  }
+
+  # if(sec_date_axis) {
+  #   p <- p + scale_x_date(date_breaks = date_breaks, date_labels = date_labels, sec.axis = ggplot2::sec_axis(trans = ~ .), expand = expansion(mult = c(0.01, 0.01)))
+  # } else {
+  #   p <- p + scale_x_date(date_breaks = date_breaks, date_labels = date_labels, expand = expansion(mult = c(0.01, 0.01)))
+  # }
 
   if(!missing(facet_col)) {
     facet_lab_pos <- match.arg(facet_lab_pos, c("top", "bottom", "left", "right"))
-    p <- p + facet_wrap(vars({{facet_col}}), nrow = facet_nrow, ncol = facet_ncol, strip.position = facet_lab_pos)
+    p <- p + facet_wrap(
+      vars({{facet_col}}),
+      nrow = facet_nrow,
+      ncol = facet_ncol,
+      scales = facet_scales,
+      strip.position = facet_lab_pos,
+      labeller = facet_labs
+    )
   }
 
   if(!missing(prop_col)) {
     p <- p +
       geom_line(data = df_prop, aes(y = prop / scaling_factor, colour = prop_line_colour), key_glyph = "timeseries", size = prop_line_size) +
       scale_colour_identity(name = NULL, breaks = prop_line_colour, labels = paste(prop_lab, "%"), guide = "legend") +
-      scale_y_continuous(sec.axis = ggplot2::sec_axis(~ . * scaling_factor, name = prop_lab, labels = scales::percent_format(accuracy = 1)))
+      scale_y_continuous(breaks = integer_breaks(), labels = scales::number_format(accuracy = 1), expand = expansion(mult = c(0, 0.05)),
+                         sec.axis = ggplot2::sec_axis(~ . * scaling_factor, name = prop_lab, labels = scales::percent_format(accuracy = 1)))
+  } else {
+    p <- p +
+      scale_y_continuous(breaks = integer_breaks(), labels = scales::number_format(accuracy = 1), expand = expansion(mult = c(0, 0.05)))
   }
 
   p <- p +
-    ggthemes::scale_fill_tableau(na.value = group_na_colour) +
+    ggthemes::scale_fill_tableau(palette = "Tableau 10", na.value = group_na_colour) +
     labs(x = date_lab, y = y_lab, fill = group_lab, caption = caption, title = title, subtitle = subtitle) +
     guides(fill = guide_legend(order = 1), colour = guide_legend(order = 0)) +
-    theme_light() +
-    theme(legend.position = "top",
-          panel.grid.major.x = element_blank(),
-          panel.grid.minor.x = element_blank(),
-          strip.text = element_text(face = "bold", size = 12)
-          )
+    #theme_classic() +
+    theme(
+      legend.position = "top",
+      panel.grid.major.x = element_blank(),
+      panel.grid.minor.x = element_blank(),
+      panel.grid.minor.y = element_blank(),
+      strip.text = element_text(face = "bold", size = 10)
+    )
 
   return(p)
 }
-
--- R/utils.R
+++ R/utils.R
@@ -13,14 +13,29 @@ magrittr::`%<>%`
 #' @param week_start first day of the week to re-set to. 7 = Sunday, 1 = Monday.
 #'
 #' @export
-set_epi_week <- function(date, week_start = 1) {
+floor_week <- function(date, week_start = 1) {
   lubridate::floor_date(lubridate::as_date(date), unit = "week", week_start = week_start)
 }
 
+#' round number up or down to nearest 01, 100, 1000, etc
+#'
+#' @param x numeric vector
+#' @param y 10 will round up to nearest 10, -10 down to nearest 10 etc
+#'
 #' @export
-rounder <- function(x,y) {
+rounder <- function(x, y) {
   if(y >= 0) { x + (y - x %% y)}
   else { x - (x %% abs(y))}
+}
+
+#' @export
+integer_breaks <- function(n = 5, ...) {
+  fxn <- function(x) {
+    breaks <- floor(pretty(x, n, ...))
+    names(breaks) <- attr(breaks, "labels")
+    breaks
+  }
+  return(fxn)
 }
 
 #' @export
@@ -35,42 +50,82 @@ pyramid_limits <- function(x) {
 }
 
 #' @export
-dodge_x_labs <- function() {
-  ggplot2::guides(x = ggplot2::guide_axis(n.dodge = 2))
+pyramid_labs_pos <- function(x, f = 5) {
+  dplyr::case_when(
+    x > 0 & x < max(abs(x)) / f ~ -0.1,
+    x > 0 & x > max(abs(x)) / f ~ 1.1,
+    x < 0 & abs(x) < max(abs(x)) / f ~ 1.1,
+    x < 0 & abs(x) > max(abs(x)) / f ~ -0.1
+  )
 }
 
-#' Bin numeric vector into breaks using classInt::classIntervals
+#' @export
+pyramid_labs_colour <- function(x, f = 5, in_col = "white", out_col = "grey30") {
+  dplyr::case_when(
+    x > 0 & x < max(abs(x)) / f ~ out_col,
+    x > 0 & x > max(abs(x)) / f ~ in_col,
+    x < 0 & abs(x) < max(abs(x)) / f ~ out_col,
+    x < 0 & abs(x) > max(abs(x)) / f ~ in_col
+  )
+}
+
+#' @param n.dodge
 #'
-#' Works with dplyr::mutate to add a column of breaks
+#' @export
+dodge_x_labs <- function(n.dodge = 2) {
+  ggplot2::guides(x = ggplot2::guide_axis(n.dodge = n.dodge))
+}
+
+#' Bin numeric vector into breaks using [`classInt::classIntervals`]
 #'
-#' Breaks labels are formatted with scales::label_number_si
+#' Works with [`dplyr::mutate`] to add a column of breaks. Breaks labels are formatted with [`scales::label_number_si`]
 #'
 #' @param x numeric vector
 #' @param n number of bins
 #' @param style of class intervals. see [`classInt::classIntervals`] for details
+#' @param lab_accuracy accuracy of labels, passed to [`scales::label_number_si`]
+#' @param replace_Inf if `Inf` is your final break, replace with a + sign in the label?
+#'
 #' @export
 add_breaks <- function(x, n = 5, style = "jenks", lab_accuracy = 1, replace_Inf = TRUE) {
   style <- match.arg(style, c("fixed", "sd", "equal", "pretty", "quantile", "kmeans", "hclust", "bclust", "fisher", "jenks", "dpih", "headtails"), several.ok = FALSE)
   breaks <- classInt::classIntervals(x, n = n, style = style)
   br <- breaks$brks
-  cut(x, br, include.lowest = TRUE, labels = label_breaks(br, lab_accuracy, replace_Inf))
+  cut(x, br, include.lowest = TRUE, right = FALSE, labels = label_breaks(br, lab_accuracy, replace_Inf))
 }
 
+#' @param breaks
+#'
+#' @param lab_accuracy accuracy of labels, passed to [`scales::label_number_si`]
+#' @param replace_Inf if `Inf` is your final break, replace with a + sign in the label?
+#'
 #' @export
 label_breaks <- function(breaks, lab_accuracy = 1, replace_Inf = TRUE) {
-  labs <- sprintf("%s-%s", frmt_num(breaks[1:length(breaks) - 1], accuracy = lab_accuracy), frmt_num(breaks[2:length(breaks)], accuracy = lab_accuracy))
+  labs <- sprintf(
+    "%s-%s",
+    frmt_num(breaks[1:length(breaks) - 1], accuracy = lab_accuracy),
+    frmt_num(breaks[2:length(breaks)] - 1, accuracy = lab_accuracy)
+  )
   if(replace_Inf){
     labs <- gsub("-Inf", "+", labs)
   }
   return(labs)
 }
 
+#' @param x a number to format
+#'
+#' @param accuracy accuracy of labels, passed to [`scales::label_number_si`]
+#'
 #' @export
 frmt_num <- function(x, accuracy = 1) {
   scales::label_number_si(accuracy = accuracy)(x)
 }
 
 #' Duplicate discrete axis labels
+#'
+#' @param label_trans
+#' @param ...
+#'
 #' @export
 guide_axis_label_trans <- function(label_trans = identity, ...) {
   axis_guide <- ggplot2::guide_axis(...)
@@ -85,3 +140,25 @@ guide_train.guide_axis_trans <- function(x, ...) {
   trained
 }
 
+#' Get max and second max values of a vector
+#'
+#' @param x
+#'
+#' @export
+max_2 <- function(x) {
+  # 2 most recent epi weeks to remove from charts
+  x <- unique(x) %>% purrr::discard(is.na) %>% sort()
+  n <- length(x)
+  c(dplyr::nth(x, n-1), dplyr::nth(x, n))
+}
+
+#' Get the date of the most recent Sunday
+#'
+#' Useful for filtering data to latest full ISO week
+#'
+#' @param date
+#'
+#' @export
+get_prev_sunday <- function(date) {
+  lubridate::floor_date(as.Date(date), unit = "week", week_start = 7)
+}
